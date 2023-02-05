5d5f7efb52a0473e3621b17db9c2c40f7acf45bf
Matt <director@acidgambit.com>
2023-02-04 18:14:30 -0600

    update img source

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,7 +4,7 @@ export default component$(() => {
   return (
     <header>
       <div class="grid place-items-center py-5">
-        <img class="inline object-cover w-24 h-24 rounded-full" src="public/conair.png" alt="Profile image"/>
+        <img class="inline object-cover w-24 h-24 rounded-full" src="https://i.imgur.com/zN2G2K6.png" alt="Profile image"/>
         <h1 class="text-gray-500 text-2xl font-bold py-2 hover:text-zinc-100">
           Matt Wilder
         </h1>
