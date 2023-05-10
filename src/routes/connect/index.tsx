5067172ca56af3c74a591c36478e11b9012b3fd7
Matt <director@acidgambit.com>
2023-05-10 12:32:11 -0500

    update links

--- src/routes/connect/index.tsx
+++ src/routes/connect/index.tsx
@@ -24,7 +24,7 @@ export default component$(() => {
           <a href="https://acidgambit.substack.com/">Substack</a>
         </li>
         <li class="underline py-1 hover:text-zinc-100">
-          <a href="https://www.instagram.com/themattbrc">Instagram</a>
+          <a href="https://www.instagram.com/acidgambit">Instagram</a>
         </li>
       </ul>
     </div>
